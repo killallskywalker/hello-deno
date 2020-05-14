f70acff6de555fc468102e8de664a72471653713
KILLALLSKYWALKER <firdausdaus98@ymail.com>
2020-05-15 07:13:35 +0800

    fix silly milly mistake

--- src/http/get-index/index.ts
+++ src/http/get-index/index.ts
@@ -8,7 +8,7 @@ export async function handler (req: object) {
       'content-type': 'application/json; charset=utf8',
       'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
     },
-    object: JSON.stringify(object)
+    body: JSON.stringify(object)
   }
 }
 
